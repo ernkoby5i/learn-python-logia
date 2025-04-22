55f1b5ec0bd461cf2abd48ced7572feda6e05a45
ernkoby5i <ernkoby5i@gmail.com>
2025-04-22 21:58:21 +0200

    Rozwiązanie zadania Zadanie 2 (Redukcja) z LOGIA 15

--- GROUP 2 LEPSZA/24. 2025-04-15 2 etap logia/Zadanie 02 redukcja.py
+++ GROUP 2 LEPSZA/24. 2025-04-15 2 etap logia/Zadanie 02 redukcja.py
@@ -1,2 +1,29 @@
 # Redukcja - https://logia.oeiizk.waw.pl/logia/page.php?sr=logia15/2etap LOGIA 15 II ETAP
 
+def redukcja (l):
+    while True:
+        i = len(l)-1
+        znaleziona = False
+        while i>=1:
+            #print(l,i)
+            b = l[i]
+            a = l[i-1]
+            if a==b:
+                l.pop(i)
+                l[i-1] = (a+b)%10
+                znaleziona = True
+                i = i-2
+            else:
+                i = i-1
+        if znaleziona == False:
+            break
+
+liczba = int(input())
+l = [int(cyfra) for cyfra in str(liczba)]
+
+
+
+redukcja(l)
+#print(l)
+for i in l:
+    print(i,end="")
