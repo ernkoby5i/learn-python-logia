5fe7311d5f32471dba43bc1b3f86bcd084112823
ernkoby5i <ernkoby5i@gmail.com>
2025-04-26 08:00:12 +0200

    Rozwiązanie zadania Zadanie 3 (Redukcja) z LOGIA 15 ileliczb (nie dziala dla duzych)

--- GROUP 2 LEPSZA/24. 2025-04-15 2 etap logia/Zadanie 03 ile liczb.py
+++ GROUP 2 LEPSZA/24. 2025-04-15 2 etap logia/Zadanie 03 ile liczb.py
@@ -1,4 +1,23 @@
 # Ile liczb - https://logia.oeiizk.waw.pl/logia/page.php?sr=logia15/2etap
-2222222222
-222222222
+def f(a,b):
+    start  = 10**(a-1)
 
+    wynik = 0
+    end = 10**a-1
+
+    print(f"{start=} {end=}")
+    for i in range(start,end+1):
+        w = str(i)
+        #print(w)
+        for d in w:
+            if d == str(b):
+                #print("mam")
+                wynik = wynik+1
+
+                break
+    return wynik
+
+
+
+wynik = f(9,3)
+print(f"{wynik=}")
