dd5c2dd1b2e71309394c17c56e8de2bcb5e7f908
ernkoby5i <ernkoby5i@gmail.com>
2025-05-06 21:21:49 +0200

    lekcja 26 Palindrom

---
+++ GROUP 2 LEPSZA/26. 2025-05-06/PALINDROM.py
@@ -0,0 +1,21 @@
+#
+
+
+def MAXX(slowo):
+    n = len(slowo)
+    max_len = 0
+    wynik = slowo[0]
+
+    for i in range(n):
+        for j in range(n - 1, i, -1):
+            if slowo[i] == slowo[j]:
+                dlugosc = j - i + 1
+                if dlugosc > max_len:
+                    max_len = dlugosc
+                    wynik = slowo[i:j+1]
+                break
+
+    return wynik
+
+slowo = ("bbbaaubueytwyetrywax")
+print(MAXX(slowo))
