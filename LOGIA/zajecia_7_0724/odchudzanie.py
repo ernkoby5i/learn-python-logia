7eb61f3b1cb5287fc383ff2ae31fc7839c18ed6f
ernkoby5i <ernkoby5i@gmail.com>
2025-07-30 23:25:57 +0200

    zadanie schudnięcie

---
+++ LOGIA/zajecia_7_0724/odchudzanie.py
@@ -0,0 +1,23 @@
+def nasza_waga(wagi):
+    if len(wagi)<=1:
+        return 0
+
+    max_waga = wagi[0]
+    max_schudnuecie = 0
+
+    for waga in wagi:
+        if waga>max_waga:
+            max_waga = waga
+        if (max_waga-waga)>max_schudnuecie:
+            max_schudnuecie = (max_waga-waga)
+    return max_schudnuecie
+
+
+wagi = [6, 7, 5, 4, 2]
+print(wagi,nasza_waga(wagi))
+
+wagi = [90, -90, 111, 4, 12]
+print(wagi,nasza_waga(wagi))
+
+wagi = [1,2,0]
+print(wagi,nasza_waga(wagi))
