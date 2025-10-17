8d78db8e90db74e83e116e4fed8a184d580f7453
ernkoby5i <ernkoby5i_github@gmail.com>
2025-10-17 21:59:42 +0200

    trojkat gotowy

---
+++ Logia stacjonarna/dzien 3/Trojkat.py
@@ -0,0 +1,41 @@
+#n = int(input())
+n = 4
+#p = int(input())
+p = 3
+podstawa = n
+kamykiT = 0
+
+def mozna_zrobic_K(kamykiT):
+    """funkcja zwraca:
+       true - jezeli z tyh kamykow mozna zrobic prostokat
+       false - jezeli jakis warunek nie jet spelniony
+    """
+    mozna = True
+    if kamykiT % p != 0:
+        mozna = False
+    else:
+        m = kamykiT // p
+        if not m >= n:
+            mozna = False
+    return mozna
+
+
+# main
+for i in range(1,n+1):
+    kamykiT = kamykiT + i
+    print(i)
+    print(kamykiT)
+    print("")
+
+while not mozna_zrobic_K(kamykiT):
+    podstawa = podstawa+1
+    kamykiT = kamykiT+podstawa
+
+m = kamykiT // p
+print(f'wynik = dlugi bok prostokata = {m} krotki = {p} ilosc kamykow w srodku = {kamykiT}')
+
+
+
+
+
+
