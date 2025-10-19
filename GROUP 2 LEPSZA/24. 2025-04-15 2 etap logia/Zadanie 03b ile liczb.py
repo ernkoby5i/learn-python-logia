ce9241d5712d4224c5ccc663c34e7b1b6bcf2f4e
ernkoby5i <ernkoby5i_github@gmail.com>
2025-10-19 11:39:49 +0200

    Ile liczb

---
+++ GROUP 2 LEPSZA/24. 2025-04-15 2 etap logia/Zadanie 03b ile liczb.py
@@ -0,0 +1,34 @@
+# Ile liczb - https://logia.oeiizk.waw.pl/logia/page.php?sr=logia15/2etap
+def f(a,b):
+    start  = 10**(a-1)
+
+    wynik = 0
+    end = 10**a-1
+
+    print(f"{start=} {end=}")
+    for i in range(start,end+1):
+        w = str(i)
+        #print(w)
+        for d in w:
+            if d == str(b):
+                #print("mam")
+                wynik = wynik+1
+
+                break
+    return wynik
+
+
+
+def ile_liczb(n,cyfra):
+    ilosc_n_cyfrowych_z_zerem = 
+    ilosci_n_cyfrowych_bez_zera =
+    wynik = ilosc_n_cyfrowych_z_zerem - ilosci_n_cyfrowych_bez_zera
+    return wynik
+
+
+
+wynik = f(9,3)
+print(f"{wynik=}")
+
+wynik = ile_liczb(9,3)
+print(f"{wynik=}")
