a68fb02cc6033385431096fb45f06bacc0258703
ernkoby5i <ernkoby5i_github@gmail.com>
2025-11-01 12:43:22 +0100

    stara mozajka zrobiona

--- Logia stacjonarna/03. zadania-done/logia-2122-mozaika.py
+++ Logia stacjonarna/03. zadania-done/logia-2122-mozaika.py
@@ -17,7 +17,16 @@ def kwadrat(z, kolor):
     pu()
 
 def mozajka():
-    n = 4
+    pd()
+    fillcolor('lightblue')
+    begin_fill()
+    goto(200, 200)
+    goto(200, -200)
+    goto(-200, -200)
+    goto(-200, 200)
+    end_fill()
+
+    n = 7
     kratki = 0
     kolory = ['green','orange']
     for i in range(2,n+1):
@@ -26,7 +35,7 @@ def mozajka():
     bok = 400/kratki
 
 
-    for i in range(0,4):
+    for i in range(1):
         zero = pos()
         z = bok * 2
         for i in range(0,n-1):
@@ -36,12 +45,37 @@ def mozajka():
             right(90)
             forward(z // 2)
             left(90)
-            z = z + bok + (2 * bok)
+            PAMIENTAJ = pos()
+            z = z + (bok*2)
         goto(zero)
-        forward(600)
+        forward(400)
         right(90)
 
+    for i in range(0,3):
+        zero = pos()
+        z = bok * 2
+        for i in range(0,n-1):
+            kolor = kolory[i%2]
+            kwadrat(z,kolor)
+            forward(z // 2)
+            right(90)
+            forward(z // 2)
+            left(90)
+            z = z + (bok*2)
+        goto(zero)
+        forward(400)
+        right(90)
+
+
+    goto(PAMIENTAJ)
+
+    z = z + (bok * 2)-bok
+    kolor = 'green'
+    kwadrat(z, kolor)
+
+
 pu()
+speed(0)
 goto(-200,200)
 mozajka()
 exitonclick()
