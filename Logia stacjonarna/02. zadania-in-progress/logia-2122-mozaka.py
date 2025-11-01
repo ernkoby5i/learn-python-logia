55d64b9d4165220997e785c13d78b3ea9e92af80
ernkoby5i <ernkoby5i_github@gmail.com>
2025-11-01 11:12:36 +0100

    mozaika in progress

---
+++ Logia stacjonarna/02. zadania-in-progress/logia-2122-mozaka.py
@@ -0,0 +1,47 @@
+# https://logia.oeiizk.waw.pl/strony/archiwalne/L22_zadania_e1.pdf
+from turtle import *
+
+def kwadrat(z, kolor):
+    pd()
+    fillcolor(kolor)
+    begin_fill()
+    forward(z)
+    right(90)
+    forward(z)
+    right(90)
+    forward(z)
+    right(90)
+    forward(z)
+    right(90)
+    end_fill()
+    pu()
+
+def mozajka():
+    n = 4
+    kratki = 0
+    kolory = ['green','orange']
+    for i in range(2,n+1):
+        kratki = kratki+i
+    kratki = kratki+kratki+2
+    bok = 400/kratki
+
+
+    for i in range(0,4):
+        zero = pos()
+        z = bok * 2
+        for i in range(0,n-1):
+            kolor = kolory[i%2]
+            kwadrat(z,kolor)
+            forward(z // 2)
+            right(90)
+            forward(z // 2)
+            left(90)
+            z = z + bok + (2 * bok)
+        goto(zero)
+        forward(600)
+        right(90)
+
+pu()
+goto(-200,200)
+mozajka()
+exitonclick()
